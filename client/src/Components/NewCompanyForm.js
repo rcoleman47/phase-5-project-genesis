a424d31c2ca25a47538a90cb8c33f14217330860
ryanadams <rcoleman.adams@gmail.com>
2022-07-19 17:14:03 -0500

    heroku deploy test

--- client/src/Components/NewCompanyForm.js
+++ client/src/Components/NewCompanyForm.js
@@ -33,7 +33,7 @@ export default function NewCompanyForm() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    fetch('/companies', {
+    fetch('genesis-estimator.herokuapp.com/companies', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(newCompayForm),
