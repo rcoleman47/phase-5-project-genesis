d93b4dc97cb3530afe1822fdd3e449fb4635c33a
ryanadams <rcoleman.adams@gmail.com>
2022-07-19 17:32:08 -0500

    messing with deployment

--- client/src/Components/NewCompanyForm.js
+++ client/src/Components/NewCompanyForm.js
@@ -33,7 +33,7 @@ export default function NewCompanyForm() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    fetch('genesis-estimator.herokuapp.com/companies', {
+    fetch('/companies', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(newCompayForm),
